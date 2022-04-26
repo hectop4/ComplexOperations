713b903bcef1631a958ed3f38758dd22b9a37376
hector219 <hectorjosepuentesruiz@gmail.com>
2022-04-26 14:43:02 -0500

    Implementacion de operaciones basicas

--- Pj1-complexOperations.py
+++ Pj1-complexOperations.py
@@ -4,4 +4,23 @@ print("a+bj      Siendo a,b numeros Reales")
 z1=complex(input("Ingrese un numero complejo 1 (cambie la 'i' por una 'j'):"))
 z2=complex(input("Ingrese un numero complejo 2 (cambie la 'i' por una 'j'):"))
 
-
+x=input("""Ahora para poder realizar una operacion con los numeros 
+usados anteriormente Ingrese el numero respectivo de la operacion
+1- Suma
+2- Resta
+3- Multiplicacion
+4- Division:
+""")
+if x=="1":
+   print("z1+z2="+str(z1+z2))
+elif x=="2":
+   print("z1-z2="+str(z1-z2))
+elif x=="3":
+   print("z1*z2="+str(z1*z2))
+elif x=="4":
+   if z2==0:
+      print("Indeterminado")
+   else:
+      print("z1/z2="+str(z1/z2))
+else:
+   print("Por favor ingrese una opcion valida")
