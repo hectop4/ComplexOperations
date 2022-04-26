5461bbaecdb096ba44d58399c55bd045e84de2f5
hector219 <hectorjosepuentesruiz@gmail.com>
2022-04-26 14:59:24 -0500

    Agregando inicializacion y uso de funcion y correccion de formato

--- Pj1-complexOperations.py
+++ Pj1-complexOperations.py
@@ -1,26 +1,36 @@
-print("Recuerde que el formato de numeros complejos es: ")
-print("a+bj      Siendo a,b numeros Reales")
-
-z1=complex(input("Ingrese un numero complejo 1 (cambie la 'i' por una 'j'):"))
-z2=complex(input("Ingrese un numero complejo 2 (cambie la 'i' por una 'j'):"))
-
-x=input("""Ahora para poder realizar una operacion con los numeros 
-usados anteriormente Ingrese el numero respectivo de la operacion
-1- Suma
-2- Resta
-3- Multiplicacion
-4- Division:
+def operaciones(z1,z2):
+   x=input("""Ahora para poder realizar una operacion con los numeros 
+usados anteriormente Ingrese el numero respectivo de la operacions
+   1- Suma
+   2- Resta
+   3- Multiplicacion
+   4- Division:
 """)
-if x=="1":
-   print("z1+z2="+str(z1+z2))
-elif x=="2":
-   print("z1-z2="+str(z1-z2))
-elif x=="3":
-   print("z1*z2="+str(z1*z2))
-elif x=="4":
-   if z2==0:
-      print("Indeterminado")
+   if x=="1":
+      print("z1+z2  =  "+str(z1+z2))
+   elif x=="2":
+      print("z1-z2  =  "+str(z1-z2))
+   elif x=="3":
+      print("z1*z2  =  "+str(z1*z2))
+   elif x=="4":
+      if z2==0:
+         print("Indeterminado")
+      else:
+         print("z1/z2=  "+str(z1/z2))
    else:
-      print("z1/z2="+str(z1/z2))
-else:
-   print("Por favor ingrese una opcion valida")
+      print("Por favor ingrese una opcion valida")
+      operaciones(z1,z2)
+
+if __name__=="__main__":
+   print("""               Recuerde que el formato de numeros complejos es:
+
+
+                   a+bj      Siendo a,b numeros Reales
+
+   """)
+   
+   z1=complex(input("Ingrese un numero complejo 1 (cambie la 'i' por una 'j'):"))
+   z2=complex(input("Ingrese un numero complejo 2 (cambie la 'i' por una 'j'):"))
+
+   operaciones(z1,z2)
+
