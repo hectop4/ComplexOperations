cde2877bf5bcadf9331e44dfb2c655180df467d2
hector219 <hectorjosepuentesruiz@gmail.com>
2022-04-26 14:19:08 -0500

    Agregando codigo para comprobar el ingreso de un numero complejo

---
+++ Pj1-complexOperations.py
@@ -0,0 +1,7 @@
+print("Recuerde que el formato de numeros complejos es: ")
+print("a+bj      Siendo a,b numeros Reales")
+
+z1=complex(input("Ingrese un numero complejo 1 (cambie la 'i' por una 'j'):"))
+z2=complex(input("Ingrese un numero complejo 2 (cambie la 'i' por una 'j'):"))
+
+
