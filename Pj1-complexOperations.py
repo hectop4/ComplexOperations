d031f97ba8ab30983e3b4a2ca15f82e175ada2f5
hector219 <hectorjosepuentesruiz@gmail.com>
2022-04-27 20:38:23 -0500

    Corrigiendo texto y agregando nuevas funciones

--- Pj1-complexOperations.py
+++ Pj1-complexOperations.py
@@ -1,3 +1,6 @@
+import cmath
+
+
 def operaciones(z1,z2):
    x=input("""Ahora para poder realizar una operacion con los numeros 
 usados anteriormente Ingrese el numero respectivo de la operacions
@@ -5,6 +8,9 @@ usados anteriormente Ingrese el numero respectivo de la operacions
    2- Resta
    3- Multiplicacion
    4- Division:
+   5-Conjugado:
+   6-Magnitud:
+   7-Fase:
 """)
    if x=="1":
       print("z1+z2  =  "+str(z1+z2))
@@ -17,17 +23,23 @@ usados anteriormente Ingrese el numero respectivo de la operacions
          print("Indeterminado")
       else:
          print("z1/z2=  "+str(z1/z2))
+   elif x=="5":
+      print("z1*=  "+str(z1.conjugate()))
+      print("z2*=  "+str(z2.conjugate()))
+   elif x=="6":
+      print("|z1|=  "+str(abs(z1)))
+      print("|z2|=  "+str(abs(z2)))
+   elif x=="7":
+      print("z1 fase=  "+str(cmath.phase(z1))+" Radianes")
+      print("z2 fase=  "+str(cmath.phase(z2))+" Radianess")
    else:
       print("Por favor ingrese una opcion valida")
       operaciones(z1,z2)
 
 if __name__=="__main__":
-   print("""               Recuerde que el formato de numeros complejos es:
-
-
-                   a+bj      Siendo a,b numeros Reales
-
-   """)
+   print("""Recuerde que el formato de numeros complejos es:
+   a+bj
+   Siendo a,b numeros Reales""")
    
    z1=complex(input("Ingrese un numero complejo 1 (cambie la 'i' por una 'j'):"))
    z2=complex(input("Ingrese un numero complejo 2 (cambie la 'i' por una 'j'):"))
